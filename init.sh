67ec938405e8bae58c4d9aee0df9cb4003b03a94
AntXinyuan <ant_xinyuan@163.com>
2020-05-08 10:31:15 +0800

    更新了项目初始化脚本，但是仍然存在找不到lorun模块的问题

--- init.sh
+++ init.sh
@@ -6,8 +6,16 @@ mkdir data/judge
 mkdir data/media
 mkdir data/temp
 mkdir data/test_case
+
+# 创建虚拟环境
+python -m venv venv
+
 # 安装项目依赖
-pip install -r requirements.txt
+cd ./judger/lorun
+../../venv/bin/python ./setup.py install
+cd ../..
+venv/bin/pip install -r requirements.txt
+
 # 迁移数据库
-python manage.py makemigrations
-python manage.py migrate
+venv/bin/python manage.py makemigrations
+venv/bin/python manage.py migrate
